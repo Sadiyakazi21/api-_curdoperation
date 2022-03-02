7ca594ce5ce0272a4fae404ca7b83b8f41da6a99
Sadiyakazi21 <89586604+Sadiyakazi21@users.noreply.github.com>
2022-03-02 13:25:49 +0530

    Topic and Course api

---
+++ demo/src/main/java/com/example/demo/course/CourseRepository.java
@@ -0,0 +1,12 @@
+ package com.example.demo.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+	List<Course> findByTopicId(String id);
+	
+	//
+}
